c3c2d7a16d760eaf90d784549d6056469bbadafe
mertsekerunity <mertseker9393@gmail.com>
2025-01-22 15:17:35 +0100

    new level added.
    new level added.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    [SerializeField] float levelLoadDelay = 2f;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Obstacle":
+                Debug.Log("Hit Obstacle");
+                StartCrashSequence();
+                break;
+            case "Start":
+                Debug.Log("Start Platform");
+                break;
+            case "Finish":
+                Debug.Log("Finish Platform");
+                StartSuccessSequence();
+                break;
+            default:
+                Debug.Log("Hit ground");
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void StartSuccessSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        Invoke(nameof(LoadNextLevel), levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        Invoke(nameof(ReloadLevel), levelLoadDelay);
+    }
 }
